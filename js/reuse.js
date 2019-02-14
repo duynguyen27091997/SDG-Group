f2c2aacb4c2c4dcf276f9a93ac314ead19301801
Duy Nguyen <wasahara11111@gmail.com>
2019-02-14 15:52:20 +0700

    add login form

--- js/reuse.js
+++ js/reuse.js
@@ -1,3 +1,4 @@
+//show phần login cho mobile
 $('.btn-login').click(function () {
     $('.btn-login__ul').slideToggle('200');
 });
@@ -5,6 +6,11 @@ let btnToggle = $(".btn-toggle");
 btnToggle.click(function () {
     $('.header__list').slideToggle('200');
 });
+//chuyển động cho button show menu mobile
+btnToggle.click(function () {
+    $(this).toggleClass('toggle');
+});
+//về đầu trang
 $(document).on("scroll",function () {
     if (window.pageYOffset>=400){
         $('.contact-right').show();
@@ -19,6 +25,28 @@ $(".btn-back-to-top").click(function () {
         scrollTop: 0
     }, 700);
 });
-btnToggle.click(function () {
-    $(this).toggleClass('toggle');
+//js form đăng kí đăng nhập
+$(".header__btn-login").click(function () {
+    event.defaultPrevented;
+    $("#login").fadeIn();
+});
+$(".header__btn-sign").click(function () {
+    event.defaultPrevented;
+    $("#sign").fadeIn();
+});
+$(".btn-account-close").click(function () {
+   $('.account').fadeOut();
+});
+$('.btn-sign').click(function () {
+   $('#sign').fadeIn();
+   $('.account').not('#sign').fadeOut();
+})
+;$('.btn-log').click(function () {
+   $('#login').fadeIn();
+   $('.account').not('#login').fadeOut();
+});
+$(".btn-forget-pass").click(function (e) {
+    e.preventDefault();
+    $("#forget-pass").fadeIn();
+    $('.account').not('#forget-pass').fadeOut();
 });
